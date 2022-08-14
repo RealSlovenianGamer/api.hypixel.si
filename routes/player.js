749cb699d4c891515184001f85fdd7e8df9c7f41
Marko <marko.a.medvesek@gmail.com>
2022-08-14 15:51:04 +0200

    beta v1.1.0
    Changelog:
    - removed useless VIRI.md file
    - updated .gitignore
    - celotno prenovil cache sistem
    - dodal 300ms sleepa pred vsakim API requestom zato da se ga ne preobremeni :)

--- routes/player.js
+++ routes/player.js
@@ -11,11 +11,23 @@ router.get('/', async (req,res) => {
 router.get('/uuid/:uuid/', isciUuid);
 router.get('/ime/:ime/', isciIme);
 
+let cachedIgralci = [];
 let cache = [];
+let UUIDcache = [];
+
 function shrani(ime, data) {
-  oldime = ime;
+  cachedIgralci.push(ime);
   cache[ime] = data;
-  console.log('Shranil podatke za', ime);
+  console.log('dodal', ime, 'v cache');
+}
+function UUIDshrani(uuid, data){
+  cachedIgralci.push(uuid);
+  UUIDcache[uuid] = data;
+  console.log('dodal', uuid, 'v cache');
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function isciIme(request, response) {
@@ -23,11 +35,13 @@ async function isciIme(request, response) {
     var ime = request.params.ime;
     //naredi check če je bilo isto ime pravkar iskano
     //če ja potem s prej shranjenih podatkov iz cache uporabi stare podatke
+    await sleep(300);
     fetch('https://api.hypixel.net/player?key=c5b34550-a69b-4ed1-a220-b155bdfa3718&name=' + ime)
           .then(response => response.json())
           .then(data => {
+              console.log('GET Request za', ime);
               if(!data.success) {
-                if(ime == oldime){
+                if(cachedIgralci.includes(ime)){
                   response.json(cache[ime]);
                 } else {
                   response.json(data)
@@ -44,13 +58,30 @@ async function isciIme(request, response) {
 async function isciUuid(request, response) {
   try {
   var uuid = request.params.uuid;
+  await sleep(3000);
   fetch('https://api.hypixel.net/player?key=c5b34550-a69b-4ed1-a220-b155bdfa3718&uuid=' + uuid)
         .then(response => response.json())
         .then(data => {
-            response.json(data);
-        });
+          console.log('GET Request za', uuid);
+          if(!data.success) {
+            if(cachedIgralci.includes(uuid)){
+              response.json(UUIDcache[uuid]);
+            } else {
+              response.json(data)
+            }
+          } else {
+          UUIDshrani(uuid, data);
+          response.json(data);
+        }
+      });
   } catch(error){
     console.log(error);
   }}
 
+// router.get('/uuid', async (req,res) => {
+//   res.json(UUIDcache)
+// })
+// router.get('/ime', async (req,res) => {
+//   res.json(cache)
+// })  
 module.exports = router
