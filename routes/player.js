60d3e67afb381791879d35e4f25f98868347b33d
Marko <marko.a.medvesek@gmail.com>
2022-08-15 12:14:38 +0200

    beta v1.3.0
    Changelog:
    - npm install node-cache axios
    - izboljšal cache sistem iz 400ms -> 20ms
    - pobrisal staro kodo

--- routes/player.js
+++ routes/player.js
@@ -4,81 +4,68 @@ require('dotenv').config()
 const API_KEY = process.env.KEY
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const path = require('path');
-
-router.get('/uuid/:uuid/', isciUuid);
-router.get('/ime/:ime/', isciIme);
+//ustvarjanje predpomnilnika
+const axios = require("axios");
+const NodeCache = require("node-cache");
+const cache = new NodeCache({ stdTTL: 300 });
 
 router.get('/', async (req,res) => {
   res.status(404).send('Error 404');
   //res.sendFile(path.join(__dirname, '../website/index.html'));
 })
 
-let cachedIgralci = [];
-let cache = [];
-let UUIDcache = [];
-
-function shrani(ime, data) {
-  cachedIgralci.push(ime);
-  cache[ime] = data;
-  console.log('dodal', ime, 'v cache');
-}
-function UUIDshrani(uuid, data){
-  cachedIgralci.push(uuid);
-  UUIDcache[uuid] = data;
-  console.log('dodal', uuid, 'v cache');
-}
-
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function isciIme(request, response) {
-    try {
-    var ime = request.params.ime;
-
-    //naredi check če je bilo isto ime pravkar iskano
-    //če ja potem s prej shranjenih podatkov iz cache uporabi stare podatke
-    await sleep(300);
-    fetch('https://api.hypixel.net/player?key=' + API_KEY + '&name=' + ime)
-          .then(response => response.json())
-          .then(data => {
-              console.log('GET Request za', ime);
-              if(!data.success) {
-                if(cachedIgralci.includes(ime)){
-                  response.json(cache[ime]);
-                } else {
-                  response.json(data)
-                }
-              } else {
-              shrani(ime, data);
-              response.json(data);
-            }
-          });
-    } catch(error){
-      console.log(error);
-    }}
-
-async function isciUuid(request, response) {
+//cache
+const verifyCache = (req, res, next) => {
   try {
-  var uuid = request.params.uuid;
-  await sleep(300);
-  fetch('https://api.hypixel.net/player?key=' + API_KEY + '&uuid=' + uuid)
-        .then(response => response.json())
-        .then(data => {
-          console.log('GET Request za', uuid);
-          if(!data.success) {
-            if(cachedIgralci.includes(uuid)){
-              response.json(UUIDcache[uuid]);
-            } else {
-              response.json(data)
-            }
-          } else {
-          UUIDshrani(uuid, data);
-          response.json(data);
-        }
-      });
-  } catch(error){
-    console.log(error);
-  }}
+    var id = req.params.uuid;
+    if (cache.has(id)) {
+      return res.status(200).json(cache.get(id));
+    }
+    return next();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+const verifyCacheIme = (req, res, next) => {
+  try {
+    var ime = req.params.ime;
+    if (cache.has(ime)) {
+      return res.status(200).json(cache.get(ime));
+    }
+    return next();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+router.get("/uuid/:uuid", verifyCache, async (req, res) => {
+  try {
+    var uuid = req.params.uuid;
+    const { data } = await axios.get('https://api.hypixel.net/player?key=' + API_KEY + '&uuid=' + uuid);
+    console.log('Iskal uuid', uuid);
+    cache.set(uuid, data);
+    return res.status(200).json(data);
+  }catch ({ response }) {
+    console.error('Error', response.status, 'za "'+ uuid + '"');
+    return res.sendStatus(response.status);
+  }
+});
+
+router.get("/ime/:ime", verifyCacheIme, async (req, res) => {
+  try {
+    var ime = req.params.ime;
+    const { data } = await axios.get('https://api.hypixel.net/player?key=' + API_KEY + '&name=' + ime);
+    console.log('Iskal ime', ime);
+    cache.set(ime, data);
+    return res.status(200).json(data);
+  } catch ({ response }) {
+    console.error('Error', response.status, 'za "'+ ime + '"');
+    return res.sendStatus(response.status);
+  }
+});
 
 module.exports = router
